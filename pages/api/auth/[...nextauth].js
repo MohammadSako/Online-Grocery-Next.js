c9f2ac4b7acc36bab162b56b0218fa613c9e77c2
MohammadSako <71136910+MohammadSako@users.noreply.github.com>
2023-06-04 16:04:28 +0300

    add more two login methods

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -14,6 +16,15 @@ export const authOptions = {
           response_type: "code",
         },
       },
+    }),
+
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
     }),
   ],
 
@@ -40,8 +51,7 @@ export const authOptions = {
       return session;
     },
   },
-  secret: process.env.JWT_SECRET
+  secret: process.env.JWT_SECRET,
 };
-
 
 export default NextAuth(authOptions);
