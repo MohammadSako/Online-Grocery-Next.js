e58b5ae19ddecffd60aef92f1fafd9b8c9642897
MohammadSako <71136910+MohammadSako@users.noreply.github.com>
2023-07-22 16:03:37 +0300

    fix3

--- pages/api/update-product.js
+++ pages/api/update-product.js
@@ -9,22 +9,26 @@ async function Handler(req, res) {
       price: req.body.price,
       description: req.body.description,
     };
-
-    const client = await MongoClient.connect(
-      "mongodb+srv://sakodatabase:EYmcsgXd4txjPb9L@cluster1.ksjs9y2.mongodb.net/products?retryWrites=true&w=majority"
-    );
-    const db = client.db();
-    const productsCollection = db.collection("products");
-
-    const result = await productsCollection.updateOne(
-      { _id: ObjectId(id) },
-      {
-        $set: data,
-      }
-    );
-    console.log(result);
-    client.close();
-    res.status(201).json({ message: "Successfully Data Updated.." });
+    try {
+      const client = await MongoClient.connect(
+        "mongodb+srv://sakodatabase:EYmcsgXd4txjPb9L@cluster1.ksjs9y2.mongodb.net/products?retryWrites=true&w=majority"
+      );
+      const db = client.db();
+      const productsCollection = db.collection("products");
+      const result = await productsCollection.updateOne(
+        { _id: ObjectId(id) },
+        {
+          $set: data,
+        }
+      );
+      console.log(result);
+      client.close();
+      res.status(201).json({ message: "Successfully Data Updated.." });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error, message: "Not successfully Data Updated.." });
+    }
   }
 }
 export default Handler;
