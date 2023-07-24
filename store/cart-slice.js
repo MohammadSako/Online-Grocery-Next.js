ce4fbd1cc0d35c73317cbf949c563a40b23177c7
MohammadSako <71136910+MohammadSako@users.noreply.github.com>
2023-07-24 18:05:37 +0300

    fix

--- store/cart-slice.js
+++ store/cart-slice.js
@@ -1,6 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { parseCookies, setCookie } from "nookies";
-
 
 const cartSlice = createSlice({
   name: "cart",
@@ -16,24 +14,21 @@ const cartSlice = createSlice({
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
       if (!existingItem) {
-        state.items = [...state.items, {
-          id: newItem.id,
-          price: newItem.price,
-          quantity: 1,
-          totalPrice: newItem.price,
-          name: newItem.title,
-          image: newItem.image,
-        }];
+        state.items = [
+          ...state.items,
+          {
+            id: newItem.id,
+            price: newItem.price,
+            quantity: 1,
+            totalPrice: newItem.price,
+            name: newItem.title,
+            image: newItem.image,
+          },
+        ];
       } else {
         existingItem.quantity++;
-        // existingItem.totalPrice += newItem.price;
         existingItem.totalPrice = existingItem.price * existingItem.quantity;
       }
-
-      // setCookie(null, 'cartItems', state.items, {
-      //   maxAge: 86400, // Cookie expiration time in seconds
-      //   path: '/', // Cookie path
-      // });
     },
 
     removeItemFromCart(state, action) {
