daa68fdb8c8cbc32447a8c02a3b068673029f665
MohammadSako <71136910+MohammadSako@users.noreply.github.com>
2023-07-29 12:33:38 +0300

    cookie test undo

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import Layout from "../components/products/layout/Layout";
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -29,7 +29,7 @@ const App = ({ Component, pageProps: { session, ...pageProps } }) => {
         );
       });
     };
-  }, [cookies]);
+  }, []);
 
   return (
     <Provider store={store}>
