19c590fdca6ba26c731251e0a816362832b39646
MohammadSako <71136910+MohammadSako@users.noreply.github.com>
2023-07-29 18:31:29 +0300

    test

--- components/ProductsItem/ProductsList.js
+++ components/ProductsItem/ProductsList.js
@@ -1,8 +1,34 @@
 import { Container, Row } from "react-bootstrap";
 import Products from "../products/Products";
 import React from "react";
+import store from "../store/index";
+import getCookies from "../util/getCookies";
+import { cartActions } from "../store/cart-slice";
+import { useEffect } from "react";
+
 
 const ProductsList = (props) => {
+  const cookies = getCookies();
+
+  useEffect(() => {
+    return () => {
+      const items = cookies?.["cartItems"]
+        ? JSON.parse(cookies?.["cartItems"])
+        : [];
+
+      items?.forEach(({ id, title, price, description, image }) => {
+        store.dispatch(
+          cartActions.addItemToCart({
+            id,
+            title,
+            price,
+            description,
+            image,
+          })
+        );
+      });
+    };
+  }, []);
   return (
     <Container>
         <Row>
